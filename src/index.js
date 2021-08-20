d90e29c29f1d4bda856ad4a544eab38e1ea2e407
anna-left <87468994+anna-left@users.noreply.github.com>
2021-08-20 21:54:17 +0300

    feat: realised function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+    let openBrack = [];
+    let pairBrack = {};
+    conf.forEach((element) => {
+        openBrack.push(element[0]);
+        pairBrack[`${element[1]}`] = element[0];
+    })
+    let stackBrack = [];
+    for (let i = 0; i < strBrack.length; i++) {
+        let curSymb = strBrack[i];
+        //если это открывающая скобка - кладем ее в стек
+        if (openBrack.includes(curSymb)) {
+            stackBrack.push(curSymb);
+        }
+        else { //если закрывающая
+            //стек пуст - скобка непарная - ошибка
+            if (stackBrack.length === 0) {
+                return false;
+            }
+            //находим верхний элемент в стеке
+            let topElStack = stackBrack[stackBrack.length - 1];
+            // ищем парную (открывающую) скобку к нашему текущему элементу
+            // и если это и есть верхний элемент стека, обе скобки удаляем
+            if (pairBrack[curSymb] === topElStack) {
+                stackBrack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stackBrack.length === 0;
 }
